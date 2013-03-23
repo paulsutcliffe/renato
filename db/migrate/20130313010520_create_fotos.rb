03bfbadb5ff90954ac40185c337716da74407363
Ricardo <marcorcp1@hotmail.com>
2013-03-23 00:58:25 -0500

    fotos will be visible by category type and if public is true

--- db/migrate/20130313010520_create_fotos.rb
+++ db/migrate/20130313010520_create_fotos.rb
@@ -15,3 +15,7 @@ class CreateFotos < ActiveRecord::Migration
     drop_table :fotos
   end
 end
+
+
+# http://stackoverflow.com/questions/10260153/rails-saving-0-instead-of-string-from-model-collection-select
+# categoria_id es string para pasarlo como string en vez de como integer
